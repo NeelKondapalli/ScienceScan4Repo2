6d79f2651651141f10bdc3c3e457a5123ae6ef97
NeelKondapalli <neel2h06@gmail.com>
2024-06-18 00:37:34 -0700

    commit test

--- ScienceScan4/ViewController.swift
+++ ScienceScan4/ViewController.swift
@@ -6,11 +6,14 @@
 //
 
 import UIKit
+import RealmSwift
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        Realm()
         
         
     }
